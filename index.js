68ec60bfac603361600c84e175b3b720e2a0e95c
abdussami01 <samaniabdussami01@gmail.com>
2021-12-27 23:40:14 +0530

    day 1 learning

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html',function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('Error: file not found in this page')
+        }else{
+            res.write(data)
+        }
+    })
+    res.end()
+})
+
+server.listen(port,function(error){
+    if(error){
+        console.log('something went wrong',error)
+
+    }
+    else{
+        console.log('everything is fine ' + port)
+    }
+})
